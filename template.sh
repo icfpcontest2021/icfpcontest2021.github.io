68e3d4ab92f2f347e044e9f5ceae7921bc7f1619
Jasper Van der Jeugt <m@jaspervdj.be>
2021-07-13 23:16:02 +0200

    Scores are not telephone numbers

--- template.sh
+++ template.sh
@@ -9,6 +9,7 @@ cat - <<EOF
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="format-detection" content="telephone=no" />
     <title>$TITLE</title>
     <link rel="stylesheet" href="style.css">
     <link href="feed.xml" type="application/atom+xml" rel="alternate" title="Atom feed">
