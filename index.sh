8aff2de4440d11ff1c10da6011cc8774e94a3ca3
Jasper Van der Jeugt <m@jaspervdj.be>
2021-07-14 21:40:08 +0200

    Add WinterMUTE writeup

--- index.sh
+++ index.sh
@@ -4,8 +4,7 @@ set -o nounset -o errexit -o pipefail
 cat - <<EOF
 <h1>ICFP Contest 2021</h1>
 <p>
-  ICFP Contest 2021 will take place<br>
-  12:00 PM (noon) Friday 9 July - 12:00 PM (noon) Monday 12 July UTC.
+  ICFP Contest 2021 took place 12:00 PM Friday 9 July - 12:00 PM Monday 12 July UTC.
 </p>
 <img style="width: 80%; max-width: 200px" alt="ICFP Contest 2021" src="images/logo.svg"><br>
 EOF
